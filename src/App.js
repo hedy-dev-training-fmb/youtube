6fab50da0c616ea49ca524064c043d2048f52caa
FerMBernal <fermartinb@gmail.com>
2021-07-28 19:52:35 -0300

    modificacion lama-react con caracteristicas de project-thelma

--- src/App.js
+++ src/App.js
@@ -9,38 +9,44 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Transactions from "./pages/transactions/transactions";
 
 function App() {
   return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="fondo">
+      <Router>
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/users">
+              <UserList />
+            </Route>
+            <Route path="/user/:userId">
+              <User />
+            </Route>
+            <Route path="/newUser">
+              <NewUser />
+            </Route>
+            <Route path="/products">
+              <ProductList />
+            </Route>
+            <Route path="/product/:productId">
+              <Product />
+            </Route>
+            <Route path="/newproduct">
+              <NewProduct />
+            </Route>
+            <Route path="/transactions">
+              <Transactions />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>  
   );
 }
 
